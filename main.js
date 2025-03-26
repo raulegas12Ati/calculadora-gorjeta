d0cb11b5f66da66b1b056a21caa2d20f5a9ddb71
webdesing369 <webdesing369@gmail.com>
2025-03-26 23:05:09 +0000

    Função para receber o valor da conta (bill)e função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,12 @@
+let bill = 0
+let tipPorcentagem = 0
+let numberOfPeople = 0
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeoploValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
