3f6f56ea421812a4d10022113d2e59f5061e97f3
webdesing369 <webdesing369@gmail.com>
2025-03-27 13:41:49 +0000

    função para calcular a gorjeta e o total por pessoa finalizada

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let bill = 0
-let tipPorcentagem = 0
+let tipPercentage = 0
 let numberOfPeople = 0
 let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 }
 
 function receiveNumberOfPeoploValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
 function receiveTipPercetageValue(value){
@@ -22,10 +24,11 @@ function receiveTipPercetageValue(value){
         customTipInput.value = ""
     }
 
-    tipPorcentagem = value / 100
+    tipPercentage = value / 100
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+    calculate()
 }
 
 function receiveCustomTipPercentageValue(){
@@ -35,4 +38,20 @@ function receiveCustomTipPercentageValue(){
     }
 
     tipPorcentagem = document.querySelector("#custom-tip").valueAsNumber / 100
+    calculate()
+}
+
+function calculate(){
+    if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+        let amountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    }
+    else{
+        console.log("preencha tudo")
+    }
 }
