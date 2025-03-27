2e77b34965455d40d16f69b2b5acb3a9844b06f0
webdesing369 <webdesing369@gmail.com>
2025-03-27 12:52:01 +0000

    Recebendo a porcentagem da gorjeta ao clicar em qualquer um dos botões

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPorcentagem = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
@@ -8,5 +9,16 @@ function receiveBillValue() {
 
 function receiveNumberOfPeoploValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercetageValue(value){
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    tipPorcentagem = value / 100
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
 }
 
