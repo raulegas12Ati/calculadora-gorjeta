e455f6c3e9d56d4d7c68ff4eeae62b8c24c8ba29
webdesing369 <webdesing369@gmail.com>
2025-03-27 13:10:57 +0000

    função para receber o valor customizado da porcentagem da gorjeta

--- main.js
+++ main.js
@@ -16,9 +16,23 @@ function receiveTipPercetageValue(value){
         buttonSelected.classList.remove("button-selected")
     }
 
+    let customTipInput = document.querySelector("#custom-tip")
+
+    if(customTipInput!== ""){
+        customTipInput.value = ""
+    }
+
     tipPorcentagem = value / 100
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
 }
 
+function receiveCustomTipPercentageValue(){
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+
+    tipPorcentagem = document.querySelector("#custom-tip").valueAsNumber / 100
+}
