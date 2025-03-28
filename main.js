a0186c11cdaea8b99bf9ec37e9122309781b0448
webdesing369 <webdesing369@gmail.com>
2025-03-28 15:51:15 +0000

    função para resetar as informações da pagina e finalização do projeto

--- main.js
+++ main.js
@@ -14,9 +14,7 @@ function receiveNumberOfPeoploValue(){
 }
 
 function receiveTipPercetageValue(value){
-    if(buttonSelected !== null){
-        buttonSelected.classList.remove("button-selected")
-    }
+    removeButtonSelectedClass()
 
     let customTipInput = document.querySelector("#custom-tip")
 
@@ -32,10 +30,8 @@ function receiveTipPercetageValue(value){
 }
 
 function receiveCustomTipPercentageValue(){
-    if(buttonSelected !== null){
-        buttonSelected.classList.remove("button-selected")
-        buttonSelected = null
-    }
+    removeButtonSelectedClass()
+    buttonSelected = null
 
     tipPorcentagem = document.querySelector("#custom-tip").valueAsNumber / 100
     calculate()
@@ -53,5 +49,32 @@ function calculate(){
     }
     else{
         console.log("preencha tudo")
+    }
+}
+
+function reset(){
+    bill = 0
+    document.querySelector("#bill").value = ""
+
+    numberOfPeople = 0
+    document.querySelector("#people").value = ""
+
+    tipPercentage = 0
+    removeButtonSelectedClass()
+
+    let customTipInput = document.querySelector("#custom-tip")
+
+    if(customTipInput!== ""){
+        customTipInput.value = ""
+    }
+
+    document.querySelector(".amount strong").innerText = "$0.00"
+    document.querySelector(".total strong").innerText = "$0.00"
+
+}
+
+function removeButtonSelectedClass(){
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
     }
 }
